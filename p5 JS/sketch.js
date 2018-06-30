42832b7874bd09faae9f65260198d7abd20edc25
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-06-30 14:55:42 +0530

    changed folder name

---
+++ p5 JS/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    //Draw rect
+     rect(0, 0, 80, 80);
+    //line function
+    line(30, 20, 85, 20);
+    stroke(122);
+    line(85, 20, 85, 75);
+    stroke(255);
+    line(85, 75, 30, 75);
+    //colors 3 types are color background(), stroke(), fill()
+}
