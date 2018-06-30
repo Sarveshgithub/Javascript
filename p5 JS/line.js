8d0613020897757ce00f979366644cdc4b604bd6
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-06-30 16:59:32 +0530

    changed the name into line.js and line.html

--- p5 JS/line.js
+++ p5 JS/line.js
@@ -1,15 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 400);
 }
 
 function draw() {
+    background(150);
     //Draw rect
-     rect(0, 0, 80, 80);
+    // rect(0, 0, 80, 80);
     //line function
     line(30, 20, 85, 20);
-    stroke(122);
     line(85, 20, 85, 75);
-    stroke(255);
     line(85, 75, 30, 75);
     //colors 3 types are color background(), stroke(), fill()
 }
