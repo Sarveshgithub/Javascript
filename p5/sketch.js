f125012f4c9e0838dd49791750427af30c9bd9c0
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-06-30 14:30:07 +0530

    line function with stroke

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,9 +1,14 @@
 function setup() {
-	createCanvas(400,400);
+    createCanvas(400, 400);
 }
 
 function draw() {
-  //Draw rect
-  rect(0, 0, 80, 80);
-  //background(0);
+    //Draw rect
+    // rect(0, 0, 80, 80);
+    //line function
+    line(30, 20, 85, 20);
+    stroke(122);
+    line(85, 20, 85, 75);
+    stroke(255);
+    line(85, 75, 30, 75);
 }
