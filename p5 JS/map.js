86db13dbcb747b4affd131824700d0f1fea9c39b
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-07-01 19:59:48 +0530

    map function

--- p5 JS/map.js
+++ p5 JS/map.js
@@ -1,13 +1,20 @@
-<html>
-  <head>
-    <style type="text/css">
-          body {margin:0; padding:0;} /* remove top and left whitespace */
-          canvas {display:block;}    /* remove scrollbars */
-          canvas:focus {outline:0;} /* remove blue outline around canvas */
-        </style> 
-    <script src="js/p5.min.js"></script>
-    <script src="circleVariables.js"></script>
-  </head>
-  <body>
-  </body>
-</html>
+var circleX;
+//setup function runs only ONCE!
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(250, 250, 100);
+    circleX = 50; // assignment operator
+}
+// Draw function runs every time i.e. LOOP!
+function draw() {
+    var valr = map(mouseX, 0, windowWidth, 0, 255);
+    var valg = map(mouseX, 0, windowWidth, 255, 0);
+    background(valr, 0, valg);
+    document.getElementById('color').innerHTML = `R : ${valr}, B : 0, G : ${valg}`;
+    noStroke();
+    fill(250, 200, 100); // last variable for the opacity
+    ellipse(mouseX, mouseY, 25, 25);
+    //own variable
+    ellipse(circleX, 200, 25, 25);
+    circleX += 1;
+}
