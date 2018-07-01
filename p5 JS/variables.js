61ab8215be22d7ac3873460611a0e76231b1383f
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-07-01 13:10:51 +0530

    How to create a Variable in P5

---
+++ p5 JS/variables.js
@@ -0,0 +1,22 @@
+var circleX;
+//setup function runs only ONCE!
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(250, 250, 100);
+    circleX = 50; // assignment operator
+}
+// Draw function runs every time i.e. LOOP!
+function draw() {
+    background(250, 250, 100);
+    noStroke();
+    fill(250, 200, 100); // last variable for the opacity
+    //ellipse(mouseX, mouseY, 25, 25);
+    //own variable
+    ellipse(circleX, 200, 25, 25);
+    circleX+=1;
+}
+
+function mousePressed() {
+    background(250, 250, 100);
+}
+// create own variable
