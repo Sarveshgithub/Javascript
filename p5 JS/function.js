9e671cc71a791c8cab5afdd4917fb3c401c4c398
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-07-05 20:17:54 +0530

    how to use function in p5 js

--- p5 JS/function.js
+++ p5 JS/function.js
@@ -1,23 +1,22 @@
 var ball = {
-    x:300,
-    y:200,
-    xspeed : 4,
-    yspeed : -4
+    x: 300,
+    y: 200,
+    xspeed: 4,
+    yspeed: 4
 }
 //setup function runs only first time
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(0);
 }
 // Draw function runs every time
 function draw() {
-	console.log('sfdsf');
-    //fill(0);
-    for (var i = 0; i < 50; i+10) {
-        for (var j = 0; j < 50; j+10) {
-        	console.log('i,j',i,j);
-            fill(random(255),0,random(255));
-            ellipse(i, j, 20, 20);
-        }
-    }
+    background(0);
+    //stroke(255);
+    //nofill();
+    ellipse(ball.x, ball.y, 20, 20);
+    if (ball.x > width || ball.x < 0) ball.xspeed = ball.xspeed * -1;
+    if (ball.y > height || ball.y < 0) ball.yspeed = ball.yspeed * -1;
+    ball.x = ball.x + ball.xspeed;
+    ball.y = ball.y + ball.yspeed;
+    console.log(ball);
 }
