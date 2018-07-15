0b9ef243f00ad95793267b02db8c60757f14ead1
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-07-15 14:32:25 +0530

    Add array of object

--- p5 JS/class_p5.js
+++ p5 JS/class_p5.js
@@ -1,30 +1,55 @@
-let bubble1, bubble2;
+let bubbles = [];
 //setup function runs only first time
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    bubble1 = new Bubble();
-    bubble2 = new Bubble();
+    for (let i = 0; i < 5; i++) {
+        let x = random(width);
+        let y = random(height);
+        let r = random(10, 50);
+        bubbles[i] = new Bubble(x, y, r);
+    }
 }
+
+function mousePressed() {
+    //   bubbles.push(new Bubble(mouseX, mouseY, random(10,20)));
+    for (let i = 0; i < bubbles.length; i++) {
+        bubbles[i].clicked(mouseX, mouseY);
+    }
+}
+// function mouseDragged(){
+//     bubbles.push(new Bubble(mouseX, mouseY, random(10,20)));
+// }
 // Draw function runs every time
 function draw() {
-    background(50);
-    bubble1.show();
-    bubble1.move();
-    bubble2.show();
-    bubble2.move();
+    background(200);
+    //bubbles.forEach(e => e.show())
+    for (let i = 0; i < bubbles.length; i++) {
+        bubbles[i].show();
+        //bubbles[i].move();
+    }
 }
 class Bubble {
-    constructor() {
-        this.x = 100,
-            this.y = 200
+    constructor(x, y, r) {
+        this.x = x,
+            this.y = y,
+            this.r = r,
+            this.brightness = 0
+    }
+    clicked(mx, my) {
+        let d = dist(mx, my, this.x, this.y);
+        if (d < this.r) {
+            if (this.brightness == 0) this.brightness = 150;
+            else this.brightness = 0;
+        }
     }
     move() {
         this.x = this.x + random(-4, 4);
         this.y = this.y + random(-4, 4);
     }
     show() {
-        noStroke();
-        fill(255);
-        ellipse(this.x, this.y, 24, 24);
+        stroke(127, 63, 120);
+        //strokeWeight(4);
+        fill(this.brightness);
+        ellipse(this.x, this.y, this.r);
     }
 }
