e378727a8cf91051d917ab505f3f9986e7de4242
Sarvesh Kumar <sarvesh.kumar@silverlinecrm.com>
2018-08-17 15:05:54 +0530

    add string and number method

--- ES6/template_literals.js
+++ ES6/template_literals.js
@@ -1,14 +1,6 @@
 
 function onload(){
 
- function callfuntion(data){
-     return data.toUpperCase();
- }   
-let data = 'I am good';    
-let template = `<h1>${callfuntion('Sarvesh')}</h1> 
-                 <p>This is my name ${data}</p> `;
-    document.getElementById('template').innerHTML = template;
-}
-
+ 
 
 onload();
