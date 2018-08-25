9f415a245442e1576f5561648f122609dff3bcbb
sarveshgithub <sarveshkumar574@gmail.com>
2018-08-25 23:37:47 +0530

    put data outside from function

--- ES6/high_order.js
+++ ES6/high_order.js
@@ -1,25 +1,27 @@
+const people = [{
+    name: 'Sarvesh',
+    age: 22
+}, {
+    name: 'Ravi',
+    age: 19
+}, {
+    name: 'Rishabh',
+    age: 20
+}, {
+    name: 'Anil',
+    age: 18
+},];
+
 function onload() {
-    const people = [{
-        name: 'Sarvesh',
-        age: 22
-    }, {
-        name: 'Ravi',
-        age: 19
-    }, {
-        name: 'Rishabh',
-        age: 20
-    }, {
-        name: 'Anil',
-        age: 18
-    }, ];
 
     let filterAge = people.filter((person) => person.age > 18);
     console.log(filterAge)
     let arrayAge = people.map((person) => person.name)
     console.log(arrayAge)
-    let totalAge = people.reduce((sum,person) => {
-        return sum+person.age;
-    },0)
+    let totalAge = people.reduce((sum, person) => {
+        return sum + person.age;
+    }, 0)
     console.log(totalAge)
+    
 }
 onload();
