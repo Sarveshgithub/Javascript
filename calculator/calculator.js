0c8e5c77d023d588a356d7eb9dae8116f4c0fd82
sarveshgithub <sarvesh.kumar574@gmail.com>
2018-09-14 15:48:20 +0530

    Add speech recnization

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,10 @@
 // Created By Sarvesh
 // Class for handle the result and statement
-var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
 class Calculator {
     constructor() {
         this.statement = '';
         this.result = '';
+        this.voiceStart = false;
     }
     update(val) {
         this.statement += val;
@@ -13,46 +12,37 @@ class Calculator {
     }
     awnswer() {
         document.getElementById('result').innerHTML = eval(this.statement);
-        var speech = new SpeechSynthesisUtterance();
-
-        // Set the text and voice attributes.
-          speech.text = `Hi sarvesh! How are you. Your awswer is ${eval(this.statement)}`
-          speech.volume = 1;
-          speech.rate = 1;
-          speech.pitch = 1;
-          window.speechSynthesis.speak(speech);
+        if (this.voiceStart) this.speak();
     }
     remove() {
-        this.statement = this.statement.slice(0,this.statement.length-1);
+        this.statement = this.statement.slice(0, this.statement.length - 1);
         document.getElementById('statement').innerHTML = this.statement;
     }
+    voice(val) {
+        this.voiceStart = val;
+        document.getElementById('checkbox').checked = this.statement;
+    }
+    speak() {
+        // Set the text and voice attributes.
+        var speech = new SpeechSynthesisUtterance();
+        speech.text = `Hi sarvesh! How are you. Your awswer is ${eval(this.statement)}`
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
+        window.speechSynthesis.speak(speech);
+    }
 }
-
 let calc = new Calculator();
-
+let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition = new SpeechRecognition();
 //function call on load
 function onload() {
-
     // document.getElementById("x").src="stop.png"
     let btnMarkup = [];
     [7, 8, 9, '/', 4, 5, 6, '*', 1, 2, 3, '-', '.', 0, 'DEL', '='].forEach(val => {
         btnMarkup.push(`<button>${val}</button>`);
     })
     document.getElementById('btns').innerHTML = btnMarkup.join('');
-    recognition.continuous = true;
-   
-    // recognition.onresult = function(event) {
-    //     console.log('start : ', event);
-    // };
-    recognition.addEventListener('result', e => {
-        const transcript  = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('')
-        if(e.results[0].isFinal)
-        console.log(transcript);
-    })
-    recognition.start();
 }
 onload();
 
@@ -60,12 +50,29 @@ document.addEventListener('click', function(val) {
     let target;
     target = val['target']['tagName'].toLowerCase();
     if (target == 'button') {
-        if(val['target']['innerHTML'] == '=')
-        calc.awnswer();
-        else if(val['target']['innerHTML'] == 'DEL')
-        calc.remove();
-        else 
-        calc.update(val['target']['innerHTML']);
-       
+        if (val['target']['innerHTML'] == '=') calc.awnswer();
+        else if (val['target']['innerHTML'] == 'DEL') calc.remove();
+        else calc.update(val['target']['innerHTML']);
     }
 })
+recognition.continuous = true;
+recognition.onresult = function(e) {
+    const transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join('')
+    if (e.results[0].isFinal) {
+        calc.update(transcript)
+        if (transcript.includes('equal')) {
+            calc.awnswer();
+        }
+    }
+};
+
+function toggle() {
+    console.log(document.getElementById('checkbox').checked);
+    if (document.getElementById('checkbox').checked) {
+        recognition.start();
+        calc.voice(true);
+    } else {
+        recognition.stop();
+        calc.voice(false);
+    }
+}
