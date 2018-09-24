ff414a82a7789f0450ddd37b72ee209d907af7ad
sarveshgithub <sarveshkumar574@gmail.com>
2018-09-24 15:25:58 +0530

    code comment

--- Sudoko P5 JS/sketch.js
+++ Sudoko P5 JS/sketch.js
@@ -1,4 +1,4 @@
-var table = [   [4, 2,   ,  ,  ,  ,  ,  , 9],
+let table = [   [4, 2,   ,  ,  ,  ,  ,  , 9],
                 [ ,  , 6, 7,  ,  ,  , 2,  ],
                 [ , 9,  ,  , 2, 5,  ,  ,  ],
                 [8,  ,  ,  ,  ,  ,  ,  ,  ],
@@ -8,14 +8,57 @@ var table = [   [4, 2,   ,  ,  ,  ,  ,  , 9],
                 [ , 5,  ,  ,  , 3, 9,  ,  ],
                 [3,  ,  ,  ,  ,  ,  , 7, 2],
             ];
-let sudoko;            
+let sudoko =[],
+    zeroElement = [];            
+
 function setup() {
 	createCanvas(360,360);
-	soduko = new Sudoko(table);	
+    for (let i = 0; i < table.length; i++) {
+        for (let j = 0; j < table.length; j++) {
+            if (table[i][j] === undefined) {
+                table[i][j] = 0;
+                zeroElement.push([i, j]);
+            }
+            sudoko.push(new Sudoko(i,j,table[i][j]));
+        }
+    }
+   console.log('sudoko',sudoko);
 }
 
 function draw() {
-	background(200);
-	soduko.printTable();
+    background(200);
+    
+    for(let i=0; i<sudoko.length;i++) {
+       // console.log(sudoko[i]);
+        sudoko[i].printTable();
+    }
+    // soduko.printTable1();
+    // soduko.main();
+        //     var nextCell;
+        //     var breakpoint = 5;
+        //    // this.printTable();
+        //    // console.table(this.newTable);
+        //     for(var i=0; i<zeroElement.length;) {
+        //         var possibleMove = false;
+        //         nextCell = zeroElement[i];
+        //         for(var j=table[nextCell[0]][nextCell[1]]+1; j<10; j++) {
+        //             table[nextCell[0]][nextCell[1]] = j;
+        //             if(this.checkSubGrid(nextCell) && this.checkColumn(nextCell) && this.checkRow(nextCell)) { 
+        //                 possibleMove = true;
+        //                 break;
+        //             }
+        //         }
+        //         if (!possibleMove) {
+        //             this.newTable[nextCell[0]][nextCell[1]] = 0;
+        //             i--;
+        //         }
+        //         else {
+        //             i++;
+        //         }
+        //         breakpoint++;
+        //        // this.printTable1();
+        //     }
+            //console.table(this.newTable);
+        
 
 }
--- Sudoko P5 JS/sudoko.js
+++ Sudoko P5 JS/sudoko.js
@@ -1,25 +1,55 @@
 class Sudoko {
-    constructor(table) {
-        this.table = table;
-    }
-    printTable() {
-        for (let i = 0; i < this.table.length; i++) {
-            for (let j = 0; j < this.table.length; j++) {
-                fill(0)
-                rect(i * 40, j * 40, 40, 40);
-                stroke(255);
-                if (this.table[i][j] === undefined) {
-                    this.table[i][j] = 0;
-                }
-                textSize(30);
-                textAlign(CENTER);
-                fill(255)
-                text(this.table[j][i], i * 41, j * 41, 40, 40);
-            }
-        }
+    constructor(row, num, col) {
+        this.row = row;
+        this.col = col;
+        this.num = num;
     }
 
-    solveSudoko() {
-        
+    printTable() {
+        fill(0)
+        rect(this.row * 40, this.col * 40, 40, 40);
+        stroke(255);
+        textSize(30);
+        textAlign(CENTER);
+        fill(255)
+        text(this.num, this.row * 41, this.col * 41, 40, 40);
+    }
+
+    checkRow(row) {
+        for (var i = 0; i < this.table; i++) {
+            if (i === row[1]) {
+                continue;
+            }
+            if (this.table[row[0]][i] === this.table[row[0]][row[1]]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkColumn(column) {
+        for (var i = 0; i < this.newTable.length; i++) {
+            if (i === column[0]) {
+                continue;
+            }
+            if (this.newTable[i][column[1]] === this.newTable[column[0]][column[1]]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkSubGrid(grid) {
+        for (var i = Math.floor(grid[0] / 3) * 3; i < Math.floor(grid[0] / 3) * 3 + 3; i++) {
+            for (var j = Math.floor(grid[1] / 3) * 3; j < Math.floor(grid[1] / 3) * 3 + 3; j++) {
+                if ((i === grid[0]) && (j === grid[1])) {
+                    continue;
+                }
+                if (this.newTable[i][j] === this.newTable[grid[0]][grid[1]]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
