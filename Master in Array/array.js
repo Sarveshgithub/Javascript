32b6cf9a1a714c4b8cbde772f4b8703e6c4acd86
sarveshgithub <sarvesh.kumar574@gmail.com>
2018-09-26 19:26:28 +0530

    add method

--- Master in Array/array.js
+++ Master in Array/array.js
@@ -1,10 +1,18 @@
 function onload() {
 // Array Method
-let arr ; 
-arr = Array.from('sarvesh');
-console.log(Array.from([1,2,3], function(val){
-    return val+1;
-}));
-}
+//--iterable method for create a array.
+//console.log(Array.from('sarvesh'));
+//console.log(Array.from([1,2,3], x=> x+1))
 
+//--Check for array isArray
+
+//console.log(Array.isArray([1,2,3]))
+//console.log(Array.isArray({name:'sarvesh'}))
+
+//--Create a new Array.of()
+//console.log(Array.of('micheal', 'sarvesh'));
+
+// Mutator Methods - These methods modify the array:
+//copyWithin()
+}
 onload();
