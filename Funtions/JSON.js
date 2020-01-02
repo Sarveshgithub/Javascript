e4732954916f1bf4c4b0220629b7c8007a11ac9b
sarveshgithub <sarveshkumar574@gmail.com>
2020-01-02 17:24:04 +0530

    add sort operation

--- Funtions/JSON.js
+++ Funtions/JSON.js
@@ -1,17 +1,47 @@
+/**
+ * Operations on JSON
+ * @author : Sarvesh 
+ */
+
 const Json = require("./TestData");
 let data = Json.data();
 
-// Search into JSON using KEY, JSON,query
+/**
+ * Returns the Array of json 
+ * @param {object} {JSON,key,query,list} The number to raise.
+ * @return {Array} filter out from JSON via KEY.
+ */
 const filterJSON = params => {
   const { JSON, key, query, list = true } = params;
   console.log(list);
   if (list) return JSON.filter(obj => obj[key] === query);
 };
 
-// Search into JSON using KEY, JSON,query
+/**
+ * Returns the Array of json 
+ * @param {object} {JSON,key,query,list} The number to raise.
+ * @return {Array} filter out from JSON via KEY.
+ */
 const searchJSON = params => {
   const { JSON, key, query } = params;
   return JSON.filter(obj => obj[key] === query);
 };
 
+/**
+ * Returns the Array of json 
+ * @param {object} {JSON,key,query,list} The number to raise.
+ * @return {Array} filter out from JSON via KEY.
+ */
+const sortJSON = params =>{
+  const {JSON , key} = params;
+  JSON.sort((a,b) => {
+    let a_val = typeof a[key] != 'number' ? a[key].toUpperCase() : a[key]
+    let b_val = typeof b[key] != 'number' ? b[key].toUpperCase() : b[key]
+    return a_val < b_val ? -1 : a_val > b_val ? 1 : 0 
+  })
+  return JSON
+}
+// For filter the data 
 console.log("data", filterJSON({ JSON: data, key: "name", query: "Ted" }));
+// For sort the data
+console.log("data", sortJSON({ JSON: data, key: "gender"}));
